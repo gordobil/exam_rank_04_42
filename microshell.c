9238fd15215446f2cb1175f1564341c2d957c020
gordobil <noahgordobil@gmail.com>
2025-04-07 12:19:09 +0200

    04_0k

--- microshell.c
+++ microshell.c
@@ -2,26 +2,41 @@
 
 int	error(char *str)
 {
-	int	i = -1;
-
-	while (str[++i] != '\0')
+	for (int i = 0; str[i] != '\0'; i++)
 		write(1, str[i], 1);
-	return (-1);
+	return (1);
+}
 
+int	cd(int argc, char **argv)
+{
+	if (argc > 2)
+		return (error("error: cd: bad arguments\n"));
+	if (chdir(argv[1]) == -1)
+		return (error("error: cd: cannot change directory to "), error(argv[1]), error("\n"));
+	return (0);
+}
+
+int	execute(int argc, char **argv, char **envp, int pipeFlag)
+{
+	if (strcmp(argv[0], "cd") == 0)
+		return (cd(argc, argv));
+	if (pipeFlag != 0)
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	int	i = 0;
 	int	j;
-	int	status = 0;
+	int	status;
 
-	while (++i < argc)
+	if (argc < 2 || !argv[1])
+		return (1);
+	for (int i = 0; i < argc, i++)
 	{
 		j = i;
-		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+		while (argv[i] && strcmp(argv[i], "|") != 0 && strcmp(argv[i], ";") != 0)
 			i++;
 		if (i != j)
-			status = execution();
+			status = execute(i - j, &argv[j], envp, (argv[i] && !strcmp(argv[i], "|")));
 	}
+	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
--- microshell.h
+++ microshell.h
@@ -1,11 +1,11 @@
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
-# include <stdlib.h>
+# include <stdio.h>
 # include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
 # include <sys/types.h>
 # include <sys/wait.h>
-# include <signal.h>
-# include <stdio.h>
 
-#endif
+# endif
