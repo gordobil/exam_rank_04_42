b31f35747725a3ccf796f5452901606673129de5
Noah Gordobil Merino <ngordobi@c3r5s2.42urduliz.com>
2025-04-07 14:06:42 +0200

    practice

--- microshell.c
+++ microshell.c
@@ -3,14 +3,14 @@
 int	error(char *str)
 {
 	for (int i = 0; str[i] != '\0'; i++)
-		write(1, str[i], 1);
+		write(1, &str[i], 1);
 	return (1);
 }
 
 int	cd(int argc, char **argv)
 {
-	if (argc > 2)
-		return (error("error: cd: bad arguments\n"));
+	if (argc != 2)
+		return (error("error: cd: bad arguments"));
 	if (chdir(argv[1]) == -1)
 		return (error("error: cd: cannot change directory to "), error(argv[1]), error("\n"));
 	return (0);
@@ -18,25 +18,42 @@ int	cd(int argc, char **argv)
 
 int	execute(int argc, char **argv, char **envp, int pipeFlag)
 {
-	if (strcmp(argv[0], "cd") == 0)
+	int	status;
+	int	fd[2];
+	int	pid;
+
+	if (strcmp("cd", argv[0]) == 0)
 		return (cd(argc, argv));
-	if (pipeFlag != 0)
+	if (pipeFlag != 0 && pipe(fd) == -1)
+		return (error("error: fatal\n"));
+	pid = fork();
+	if (pid == 0)
+	{
+		if (pipeFlag != 0 && (dup2(fd[1], STDOUT_FILENO) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return (error("error: fatal\n"), exit(1), 1);
+		argv[argc] = NULL;
+		return (execve(argv[0], argv, envp), error("error: cannot execute "), error(argv[0]), error("\n"), exit(1), 1);
+	}
+	waitpid(pid, &status, 0);
+	if (pipeFlag != 0 && (dup2(fd[0], STDIN_FILENO) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		return (error("error: fatal\n"), exit(1), 1);
+	return (status);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
+	int	status = 0;
 	int	j;
-	int	status;
 
 	if (argc < 2 || !argv[1])
 		return (1);
-	for (int i = 0; i < argc, i++)
+	for (int i = 1 ; i < argc; i++)
 	{
 		j = i;
-		while (argv[i] && strcmp(argv[i], "|") != 0 && strcmp(argv[i], ";") != 0)
+		while (i < argc && argv[i] && strcmp("|", argv[i]) != 0 && strcmp(";", argv[i]) != 0)
 			i++;
 		if (i != j)
-			status = execute(i - j, &argv[j], envp, (argv[i] && !strcmp(argv[i], "|")));
+			status = execute(i - j, &argv[j], envp, (argv[i] && !strcmp("|", argv[i])));
 	}
 	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
