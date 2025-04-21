dcbd8607695607b38f07bbffee4cd9b5d02a12e0
Noah Gordobil Merino <ngordobi@c3r7s5.42urduliz.com>
2025-04-21 12:46:48 +0200

    fixed(?

--- microshell.c
+++ microshell.c
@@ -3,7 +3,7 @@
 int	error(char *str)
 {
 	for (int i = 0; str[i] != '\0'; i++)
-		write(1, &str[i], 1);
+		write(2, &str[i], 1);
 	return (1);
 }
 
--- microshell.h
+++ microshell.h
@@ -1,11 +1,9 @@
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
-# include <stdio.h>
 # include <unistd.h>
-# include <string.h>
 # include <stdlib.h>
-# include <sys/types.h>
+# include <string.h>
 # include <sys/wait.h>
 
 # endif
